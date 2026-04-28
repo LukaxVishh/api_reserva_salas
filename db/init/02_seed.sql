5d52f8650d73275a0b5acf28c63598ac2d8ec186
cesusieh <cesux3@gmail.com>
2026-04-28 15:26:16 -0300

    Create seeds

---
+++ db/init/02_seed.sql
@@ -0,0 +1,6 @@
+USE salalivre;
+
+INSERT INTO salas (nome, bloco, capacidade, tem_projetor, ativa) VALUES
+  ('Sala 101',   'Bloco A', 30, TRUE,  TRUE),
+  ('Sala 102',   'Bloco A', 20, FALSE, TRUE),
+  ('Sala Lab 1', 'Bloco B', 25, TRUE,  FALSE);
